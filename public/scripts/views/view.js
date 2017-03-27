6a977579ae497c389e6da78f52502a35501046ae
Keeley Hammond <keeleymhammond@gmail.com>
2017-03-27 14:02:58 -0700

    Single page app and pagejs code successful

--- public/scripts/views/view.js
+++ public/scripts/views/view.js
@@ -1,5 +1,18 @@
-(function(module) {
+'use strict';
+
+(function (module) {
   const view = {};
+
+  //Added click handler for hamburger menu in here
+  view.menuAnimation = function () {
+    $('.icon-menu').on('click', function () {
+      $('.main-nav ul').slideToggle('fast');
+    })
+  }
+
+  view.initIndexPage = function () {
+    menuAnimation();
+  };
 
   module.view = view;
 })(window);
--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-var express = require('express'),
-  port = process.env.PORT || 3000,
-  app = express();
+'use strict';
 
-  app.listen(port, function() {
-    console.log(`server started on ${port}`);
-  })
+const pg = require('pg');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+console.log(process.env);
+//loads rest of CSS assets in public
+app.use(express.static('./public'));
+
+//set index.html to root
+app.get('/', function (req, res) {
+  res.sendFile('index.html', { root: '.' });
+});
+
+//app.listen to log a console message telling us what PORT is up.
+app.listen(PORT, function () {
+  console.log(`Server set to ${PORT}`);
+});
