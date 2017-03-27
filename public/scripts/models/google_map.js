a45b27884f55084843ab930f6d59607a55b58f7f
Yuval Allweil <themarquisdesheric@gmail.com>
2017-03-27 16:15:46 -0700

    Making headway with the info boxes, still formatting

--- public/scripts/models/google_map.js
+++ public/scripts/models/google_map.js
@@ -1,37 +1,50 @@
 'use strict';
 
-function initMap() {
-  const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 8,
-    center: locations[0].address
-  });
+const GOOGLE_GEOCODE_API_KEY = 'AIzaSyB3ds8lw9KjCDvLxfBCj5EpU52-5nGUe_Q';
 
-  for (var i = 0; i < locations.length; i++) {
-    let url = geocodeUrl(locations[i].title);
-    $.get(url).done(function (response) {
-      new google.maps.Marker({
-        position: response.results[0].geometry.location,
-        map: map,
-      });
-    });
-  }
-}
-
-var locations = [
+const locations = [
   {
-    title: 'Fly Awake Tea',
-    address: {lat: 45.549863, lng: -122.675790}
+    title: 'Fly Awake Tea'
   },
   {
-    title: 'Tao of Tea',
+    title: 'Tao of Tea'
   },
   {
-    title: 'Heavens Tea, School of Tea Arts',
+    title: 'Heavens Tea, School of Tea Arts'
   }
 ];
-
-const GOOGLE_GEOCODE_API_KEY = 'AIzaSyB3ds8lw9KjCDvLxfBCj5EpU52-5nGUe_Q';
 
 function geocodeUrl(title) {
   return `https://maps.googleapis.com/maps/api/geocode/json?address=${title}&key=${GOOGLE_GEOCODE_API_KEY}`;
 }
+
+function initMap() {
+  //initialize map and center it
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: {lat: 45.549863, lng: -122.675790}
+  });
+  //loop through locations array and convert title to latLng
+  for (let i = 0; i < locations.length; i++) {
+    let url = geocodeUrl(locations[i].title);
+    //set marker for each shop
+    $.get(url).done(function (response) {
+      console.log(response);
+      //create marker for shop
+      let marker = new google.maps.Marker({
+        position: response.results[0].geometry.location,
+        map: map,
+      });
+      //create info window for marker
+      let infoWindow = new google.maps.InfoWindow({
+        content: `<b>${locations[i].title}</b>
+                  <p>${response.results[0].address_components[0].short_name} ${response.results[0].address_components[1].short_name}</p>
+                  <p>${response.results[0].address_components[3].short_name}</p>
+                 `
+      });
+
+      infoWindow.open(map, marker);
+    });
+  }
+}
+
