bb88261dca42d89743194b5f319a065f6a41d066
Keeley Hammond <keeleymhammond@gmail.com>
2017-03-27 15:31:54 -0700

    Google Maps functionality starting to work! :

--- public/scripts/models/google_map.js
+++ public/scripts/models/google_map.js
@@ -6,15 +6,32 @@ function initMap() {
     center: locations[0].address
   });
 
-  var marker = new google.maps.Marker({
-    position: locations[0].address,
-    map: map,
-  });
+  for (var i = 0; i < locations.length; i++) {
+    let url = geocodeUrl(locations[i].title);
+    $.get(url).done(function (response) {
+      new google.maps.Marker({
+        position: response.results[0].geometry.location,
+        map: map,
+      });
+    });
+  }
 }
 
-const locations = [
+var locations = [
   {
     title: 'Fly Awake Tea',
     address: {lat: 45.549863, lng: -122.675790}
+  },
+  {
+    title: 'Tao of Tea',
+  },
+  {
+    title: 'Heavens Tea, School of Tea Arts',
   }
 ];
+
+const GOOGLE_GEOCODE_API_KEY = 'AIzaSyB3ds8lw9KjCDvLxfBCj5EpU52-5nGUe_Q';
+
+function geocodeUrl(title) {
+  return `https://maps.googleapis.com/maps/api/geocode/json?address=${title}&key=${GOOGLE_GEOCODE_API_KEY}`;
+}
