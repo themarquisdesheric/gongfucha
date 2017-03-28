2f2fcdbd053b00bbaf8008fe9b9e414b2caaf35d
Yuval Allweil <themarquisdesheric@gmail.com>
2017-03-28 14:43:46 -0700

    Places and Places Details APIs working and communicating happily with eachother.

--- public/scripts/models/google_map.js
+++ public/scripts/models/google_map.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const GOOGLE_GEOCODE_API_KEY = 'AIzaSyB3ds8lw9KjCDvLxfBCj5EpU52-5nGUe_Q';
 const GOOGLE_PLACES_API_KEY = 'AIzaSyABR5dsVE6XNT0UOAKI_qmSC4_p0jPShQM';
 
 const locations = [
@@ -14,15 +13,12 @@ const locations = [
     title: 'Heavens Tea, School of Tea Arts'
   }
 ];
-//this is how we ping the google geocode API with a shop name and it returns the latLng
-function geocodeUrl(title) {
-  return `https://maps.googleapis.com/maps/api/geocode/json?address=${title}&key=${GOOGLE_GEOCODE_API_KEY}`;
-}
 
+//format the URL for the call to Places API
 function placesUrl(title) {
   return `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${title}&key=${GOOGLE_PLACES_API_KEY}`
 }
-
+//format the URL for the call to Places Details API
 function placeDetailsUrl(id) {
   return `https://maps.googleapis.com/maps/api/place/details/json?placeid=${id}&key=${GOOGLE_PLACES_API_KEY}`
 }
@@ -34,26 +30,35 @@ function initMap() {
     center: {lat: 45.549863, lng: -122.675790},
     scrollwheel: false
   });
-  //loop through locations array and convert title to latLng
+
+  //loop through locations array and populate map with markers
   for (let i = 0; i < locations.length; i++) {
-    let url = geocodeUrl(locations[i].title);
-    //set marker for each shop
-    $.get(url).done(function (response) {
-      // console.log('Geocode response: ', response);
-      //create marker for shop
-      let marker = new google.maps.Marker({
-        position: response.results[0].geometry.location,
-        map: map,
-      });
-      //create info window for marker
-      let infoWindow = new google.maps.InfoWindow({
-        content: `<p>${locations[i].title}</p>
-                  <p>${response.results[0].formatted_address}</p>`,
-        maxWidth: 120
-      });
-      //click handler to open info windows
-      marker.addListener('click', function() {
-        infoWindow.open(map, marker);
+    let url = placesUrl(locations[i].title);
+    //send GET request to Places API to obtain place ID
+    $.get(`https://crossorigin.me/${url}`).done(function (response) {
+      let placeId = response.results[0].place_id;
+      let placeIdUrl = placeDetailsUrl(placeId);
+      //send GET request to Places Details API for shop info
+      $.get(`https://crossorigin.me/${placeIdUrl}`).done(function(response) {
+        console.log('Place Details response: ', response);
+
+        //create marker for shop
+        let marker = new google.maps.Marker({
+          position: response.result.geometry.location,
+          map: map,
+        });
+        //create info window for marker
+        let infoWindow = new google.maps.InfoWindow({
+          content: `<p>${locations[i].title}</p>
+                    <p>${response.result.formatted_address}</p>
+                    <p>${response.result.formatted_phone_number}</p>
+                    <p><a href="${response.result.website}">${response.result.website}</a></p>`,
+          maxWidth: 120
+        });
+        //click handler to open info windows
+        marker.addListener('click', function() {
+          infoWindow.open(map, marker);
+        });
       });
     });
   }
@@ -62,19 +67,6 @@ function initMap() {
     const center = map.getCenter();
     google.maps.event.trigger(map, 'resize');
     map.setCenter(center);
-  });
-
-
-
-  //working, now need to incorporate this into the for loop and replace geocoding API
-  let place = placesUrl(locations[0].title);
-
-  $.get(`https://crossorigin.me/${place}`).done(function(response) {
-    let placeId = response.results[0].place_id;
-    let placeIdUrl = placeDetailsUrl(placeId);
-    $.get(`https://crossorigin.me/${placeIdUrl}`).done(function(response) {
-      console.log('Place Details response: ', response);
-    });
   });
 }
 
